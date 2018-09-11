25606d8f56e9ac07b9ec69b7c9a2429dff71dfdc
Lan Qingyong <lanqy.me@gmail.com>
2018-09-11 16:13:33 +0800

    update

--- src/standard_lib.ml
+++ src/standard_lib.ml
@@ -177,3 +177,411 @@ List.iteri (Printf.printf "idendx = %d element = %d\n") [-10;3;100;50;5;20];;
 
 List.iter2;;
 List.iter2 (Printf.printf "%s = %d\n") ["x"; "y"; "z"] [1;2;3];;
+
+
+(* Array *)
+[|10; 2; 100; 25; 0; 1|];;
+let a = [|10; 2; 10; 25; 100; 0; 1|];;
+a.(0);;
+a.(1);;
+a.(4);;
+a.(10);;
+
+Array.length;;
+Array.length a;;
+Array.map;;
+Array.map (fun x -> x * 2 + 1) a;;
+a.(0) <- 56;;
+a;;
+a.(5) <- 567;;
+(*  Maps a function that takes the index of element
+    as first argument and then the element as second
+    argument.
+
+    f(index, element)
+*)
+Array.mapi;;
+Array.mapi (fun idx e -> idx, e) [|56; 2; 10; 25; 100; 567; 1|];;
+Array.to_list;;
+Array.to_list a;;
+Array.get;;
+Array.get a 0;;
+Array.get a 20;;
+
+let a = [|56; 2; 10; 100; 567; 1|];;
+Array.set;;
+Array.set a 0 15;;
+Array.set a 4 235;;
+
+Array.fold_right;;
+Array.fold_right (+) [|56; 2; 10; 25; 100; 567; 1|] 0;;
+
+Array.fold_left;;
+Array.fold_left (+) 0 [|56; 2; 10; 25; 100; 567; 1|];;
+Array.iter (Printf.printf "x = %d\n") [|56; 2; 10; 25; 100; 567; 1|];;
+(*  Applies a function that takes the index of element
+    as first argument and then the element as second
+    argument.
+
+    f(index, element)
+*)
+Array.iteri;;
+Array.iteri (Printf.printf "x[%d] = %d\n") [|56; 2; 10; 25; 100; 567; 1|];;
+
+Array.init;;
+Array.init 10 (fun x -> 5. *. float_of_int x);;
+Array.init 10 (fun x -> 5 * x + 10);;
+
+Array.make_matrix;;
+Array.make_matrix;;
+Array.make_matrix 2 3 1;;
+
+(* String *)
+
+(* Char Module *)
+
+Char.chr;;
+
+(* Acii code to Char *)
+Char.chr 99;;
+
+(* Char to Ascii Code*)
+Char.code 'a';;
+Char.code;;
+
+(* Upper Case / Lower Case *)
+
+Char.uppercase 'c';;
+Char.lowercase 'A';;
+
+(* String Module *)
+
+(* Length of String *)
+String.length "Hello world";;
+
+(* Concatenate Strings *)
+String.concat "\n" ["Hello"; "World"; "OCaml"];;
+"Hello " ^ " World" ^ " OCaml";;
+
+(* Uppercase / Lowercase *)
+String.capitalize_ascii "hello world";;
+String.lowercase_ascii "HELLO WORLD";;
+
+(* Get Character at position *)
+String.get;;
+String.get "Hello world" 0;;
+String.get "Hello world" 1;;
+
+List.map (String.get "Buffalo") [0; 1; 2; 3; 4; 5; 6];;
+
+let str = "My string";;
+str.[0];;
+str.[5];;
+
+(* Setting Characters *)
+let str = "My string";;
+(* deprecated: String.set use Bytes.set instead. *)
+
+str.[0] <- 'H';; (* Bytes.set str 0 'H';; *)
+str.[1] <- 'e';; (* Bytes.set str 1 'e';; *)
+
+(* Contains test if character is in the string *)
+String.contains;;
+String.contains "hello" 'c';;
+String.contains "hello" 'h';;
+
+(* Map Characters *)
+String.map;;
+let encode key chr = Char.chr ((Char.code chr) - key);;
+let decode key chr = Char.chr (Char.code chr + key);;
+
+let str = "Hello world";;
+String.map (encode 13) str;;
+String.map (decode 13) ";X__b\019jbe_W";;
+
+(* Str Module *)
+(* When in the toploop interactive shell *)
+#load "str.cma"
+
+(* Compile String to a regular expression*)
+Str.regexp;;
+Str.regexp ",";;
+
+(* Split String *)
+
+Str.split;;
+Str.split (Str.regexp ",") "23.232,9823,\"Ocaml Haskell FP\"";;
+Str.split (Str.regexp "[,|;]") "23.232,9823,\"Ocaml Haskell FP\"";;
+Str.split (Str.regexp "[ ]") "23.232 9823 Ocaml Haskell FP 400";;
+Str.string_before;;
+Str.string_before "Hello world ocaml" 3;;
+Str.string_before "Hello world ocaml" 11;;
+Str.string_after "Hello world ocaml" 11;;
+Str.split (Str.regexp "[ ]") "23.232 9823 Ocaml Haskell FP 400";;
+
+(* Buffer Module *)
+Buffer.create;;
+Buffer.contents;;
+Buffer.add_char;;
+Buffer.add_bytes;;
+Buffer.add_string;;
+Buffer.reset;;
+let b = Buffer.create 0;;
+Buffer.contents b;;
+Buffer.add_string b "OCaml fp rocks ! OCaml is amazing";;
+Buffer.contents b;;
+Buffer.add_string b " - OCaml is strict FP";;
+Buffer.contents b;;
+
+Buffer.length b;;
+
+Buffer.reset b;;
+Buffer.contents b;;
+
+
+let c = Buffer.create 0;;
+Buffer.add_char c 'o';;
+Buffer.add_char c 'c';;
+Buffer.add_char c 'a';;
+Buffer.add_char c 'm';;
+Buffer.add_char c 'l';;
+
+Buffer.contents c;;
+
+print_string (Buffer.contents c);;
+Buffer.nth c 0;;
+
+Buffer.nth c 0;;
+Buffer.nth c 1;;
+Buffer.nth c 2;;
+Buffer.nth c 3;;
+Buffer.nth c 4;;
+List.map (Buffer.nth c) [0;1;2;3;4];;
+let s = "hindler milner type system";;
+let b = Buffer.create 0;;
+Buffer.add_substring;;
+Buffer.add_substring b s 0 4;;
+Buffer.contents b;;
+Buffer.add_substring b s 4 5;;
+Buffer.contents b;;
+Buffer.add_substring b s 6 10;;
+Buffer.contents b;;
+Buffer.sub;;
+
+let c = Buffer.create 0;;
+Buffer.add_string c "OCaml is almost fast as C";;
+Buffer.sub c 0 1;;
+Buffer.sub c 0 5;;
+Buffer.sub c 0 10;;
+Buffer.sub c 5 15;;
+
+(* String Processing *)
+(* Splitting a string into characters. *)
+let s = "p2p peer to peer connection";;
+let arr = Array.make;;
+let arr = Array.make (String.length s) '\000';;
+for i = 0 to (String.length s - 1) do
+    arr.(i) <- s.[i]
+done;;
+
+arr;;
+
+let str2chars s = 
+    let arr = Array.make (String.length s) '\000' in
+    for i = 0 to (String.length s - 1) do
+        arr.(i) <- s.[i]
+    done;
+    arr;;
+
+str2chars "fox";;
+
+str2chars "fox" |> Array.map Char.code;;
+
+let str2chars_ s = 
+    let arr = Array.make (String.length s) '\000' in
+    for i = 0 to (String.length s - 1) do
+        arr.[i] <- s.[i]
+    done;
+    Array.to_list arr;;
+
+str2chars_ "UNIX";;
+str2chars_ "UNIX" |> List.map Char.code;;
+
+(* Functional Approach *)
+
+let str2chars s = 
+    let n = String.length s in
+    let rec aux i alist = 
+        if i = 0
+        then []
+        else s.[n - i] :: (aux (i - 1) alist)
+    in aux n [];;
+
+str2chars "UNIX";;
+str2chars "UNIX" |> List.map Char.code;;
+
+(* Extract Digits *)
+let digit_of_char c =
+    match c with
+    | '0' -> 0
+    | '1' -> 1
+    | '2' -> 2
+    | '3' -> 3
+    | '4' -> 4
+    | '5' -> 5
+    | '6' -> 6
+    | '7' -> 7
+    | '8' -> 8
+    | '9' -> 9
+    | _ -> failwith "Not a digit";;
+
+str2chars "12345" |> List.map digit_of_char;;
+str2chars "12334x" |> List.map digit_of_char;;
+let str2digits s = str2chars s |> List.map digit_of_char;;
+str2digits "1234";;
+
+(* Join Chars *)
+let cs = ['O';'C';'a';'m';'l'];;
+Buffer.add_char;;
+List.iter;;
+List.iter (Buffer.add_char b) cs;;
+Buffer.contents b;;
+let chars2str cs =
+    let b = Buffer.create 0 in
+    List.iter (Buffer.add_char b) cs;
+    Buffer.contents b;;
+
+chars2str ['O';'C';'a';'m';'l'];;
+
+(* Strip Left Chars *)
+let trim chars s =
+    let n = String.length s in
+    let b = Buffer.create 0 in
+    for i = 0 to n - 1 do
+        if List.mem s.[i] chars
+            then ()
+            else Buffer.add_char b s.[i]
+    done;
+    Buffer.contents b;;
+
+trim ['-'; '.']  "-.--.--.....-trim chars---...----.-.-" ;;
+
+(* Sys *)
+(* Command Line Arguments *)
+Sys.argv;;
+
+(* Current Directory *)
+Sys.getcwd;;
+Sys.getcwd();;
+
+(* Change Current Directory *)
+Sys.chdir;;
+Sys.chdir "/";;
+Sys.getcwd();;
+
+(* List directory *)
+Sys.readdir;;
+Sys.readdir("/");;
+
+(* Test if is directory *)
+Sys.is_directory "/etc";;
+Sys.is_directory "/etca";;
+Sys.is_directory "/etc/fstab";;
+
+(* Test if File exists *)
+Sys.file_exists;;
+Sys.file_exists "/etc";;
+Sys.file_exists "/etca";;
+
+(* Execute shell command and return exit code
+     *   0    For success
+     *  not 0 For failure
+     *)
+
+Sys.commond;;
+Sys.commond "uname -a";;
+Sys.commond "nam";;
+
+(* Constants and Flags *)
+
+(* Machine Word Size in bits *)
+Sys.word_size;;
+
+(* Machine Endianess *)
+Sys.big_endian;;
+
+(*
+     *  For Linux/BSD or OSX  --> "Unix"
+     *  Windows               --> "Win32"
+    *)
+
+Sys.os_type;;
+
+(*  Maximum length of strings and byte sequences. =
+     *   =~  16 MB
+     *)
+
+Sys.max_string_length;;
+
+(* Extra Example *)
+let (|>) x f = f x;;
+let (|<) f x = f x;;
+Sys.chdir "/boot";;
+Sys.readdir ".";;
+
+(* List only directories *)
+"." |> Sys.readdir |> Array.to_list |> List.filter Sys.is_directory;;
+
+(* List only files *)
+"." |> Sys.readdir |> Array.to_list |> List.filter (fun d -> not <| Sys.is_directory d);;
+
+(* Filename *)
+(* It provides file name combinators. *)
+#show Filename;;
+
+(** Note:  Directory Separtor, on Windows it is (\), but is written 
+in Ocaml as (\\) , but you can also use Unix path conventions, "/" 
+the root is translater as C:\\ and "/" as \\ and "." as the current
+directory.   
+*)
+
+Filename.dir_sep;;
+
+(** Concat Concat File Paths *)
+Filename.concat "/home/dummy/Document/" "blueprints.doc";;
+
+List.map (Filename.concat "/opt") ["dir1";"dir2";"dir3"];;
+
+let concat_paths pathlist = List.fold_right Filename.concat pathlist "";;
+concat_paths ["/"; "etc"; "fstab"];;
+
+(** Get the file name without path to file *)
+Filename.basename "/etc/fstab";;
+
+
+
+(** Get the path where is the file or directory*)
+Filename.dirname "/home/tux/windows/ocamlapidoc/index.html";;
+Filename.dirname "/home/tux/windows/ocamlapidoc"
+
+(** Test if file ends with extension *)
+Filename.check_suffix "test.ml" "ml";;
+Filename.check_suffix "test.ml" "sh";;
+
+"/etc"
+|> Sys.readdir
+|> Array.to_list
+|> List.filter (fun x -> Filename.check_suffix x "conf");;
+
+(* You can can create combinators to enhance the composability, 
+   reusability and modularity   
+*)
+let listdir path = Sys.readdir path |> Array.to_list;;
+let is_file_type ext fname = Filename.check_suffix fname ext;;
+listdir "/etc" |> List.filter (is_file_type ".conf");;
+
+List.map Filename.chop_extension ["file.ml"; "filex.hs";"file2.py"];;
+Filename.chop_suffix "archive.tar.gz" ".tar.gz";;
+Filename.chop_suffix "archive.tar.gz" ".tar.bz2";;
+Filename.quote "separated file name is not good.txt";;
+Filename.quote "separated file name is not good.txt" |> print_endline;;
